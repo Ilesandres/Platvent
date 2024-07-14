41f7606f6e82820097da3a8e2c48e59c4d3be92c
Andres <ilessambonibreinerandres@gmail.com>
2024-07-14 13:14:43 -0500

    err

--- php/controladores/config.php
+++ php/controladores/config.php
@@ -1,14 +1,13 @@
 <?php
 // Configuración de la base de datos
-define('DB_SERVER', 'monorail.proxy.rlwy.net');
+define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'fDTAtMfwXWpMxkjktpnhhVDxwjjQcIHB');
-define('DB_NAME', 'railway');
-define('Db_Port','16085');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'bdventa');
 
 // Conexión a la base de datos
 function conectarDB() {
-    $conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME,Db_Port);
+    $conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
     $conn->set_charset('utf8');
 
     // Verificar conexión
